aa1264eb407b5cc6ffb475bd14062488de8a556e
Philip Park <not@your.concern>
2022-11-19 02:56:00 +0100

    feat: reorganisation of code feat: added performance video

--- js/utility/utility-visibility-toggler.js
+++
@@ -1,6 +0,0 @@
-const toggleVisiblity = (target, hiddenClass) => {
-    const element = document.querySelector(target).classList;
-    element.toggle(hiddenClass);
-};
-
-export default toggleVisiblity;
